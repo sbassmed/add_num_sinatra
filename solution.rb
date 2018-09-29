51d7f95439953aedab33f675dd9a24d8746f19a0
sbassmed <sebastiancamilomarin@gmail.com>
2018-09-28 21:31:48 -0500

    cuarto intento

--- solution.rb
+++ solution.rb
@@ -4,11 +4,11 @@ get '/' do
 
   @suma = params[:item]
   @suma = 0 if @suma == nil
-
+  erb :index
 end
 
 get '/suma' do
   @suma = params[:suma].to_i + 1
   redirect "/?item=#{@suma}"
-  erb :index
+
 end
